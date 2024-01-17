8693d8054604d151d87b4e79f2ccb9de63a03e5d
patriciatiffany <patricia@chezvoila.com>
2024-01-17 11:15:28 -0500

    Exercise 4

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -22,7 +22,7 @@ function Game() {
   }
   return <>
     <GuessInput addToHistory={addToHistory}/>
-    <GuessResults results={inputHistory}/>
+    <GuessResults results={inputHistory} answer={answer}/>
   </>;
 }
 
--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -2,31 +2,28 @@ import React from "react";
 
 import { NUM_OF_GUESSES_ALLOWED, NUM_LETTERS } from "../../constants";
 import { range } from "../../utils";
+import { checkGuess } from "../../game-helpers";
 
 function Guess({ guess }) {
-  return range(NUM_LETTERS).map((i) => (
-    <span key={i} className="cell">{guess[i] || ''}</span>
-  ));
-  // if (guess === "") {
-  //   return range(NUM_LETTERS).map((i) => (
-  //     <span key={i} className="cell"></span>
-  //   ));
-  // } else {
-  //   return Array.from(guess).map((char, i) => (
-  //     <span key={i} className="cell">
-  //       {char}
-  //     </span>
-  //   ));
-  // }
+  if (guess.length === 0) {
+    return range(NUM_LETTERS).map((i) => (
+      <span key={i} className="cell"></span>
+    ));
+  } else {
+    return guess.map((d, i) => (
+      <span key={i} className={`cell ${d.status}`}>
+        {d.letter}
+      </span>
+    ));
+  }
 }
 
-function GuessResults({ results }) {
-  // console.log(range(NUM_OF_GUESSES_ALLOWED)); // [0, 1, 2, 3, 4, 5]
+function GuessResults({ results, answer }) {
   return (
     <div className="guess-results">
-      {range(NUM_OF_GUESSES_ALLOWED).map((i) => (
+      {range(NUM_OF_GUESSES_ALLOWED).map((i) => ( // [0, 1, 2, 3, 4, 5]
         <p className="guess" key={i}>
-          <Guess guess={results[i] ? results[i].value : ""} />
+          <Guess guess={results[i] ? checkGuess(results[i].value, answer) : []} />
         </p>
       ))}
     </div>
